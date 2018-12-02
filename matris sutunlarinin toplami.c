779efaf65829af6c2605639217f4f654ffa50b55
Furkan Aydoğan <furkanaydogance@gmail.com>
2018-12-03 00:38:29 +0300

    matris_sutunlar_toplami
    3x4 bir matrisin sutunlarinin toplamini gostermeye calisitim

---
+++ matris sutunlarinin toplami.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int matris[3][4];
+    int i,j;
+    int sum=0;
+    for(i=0;i < 3;i++){
+        for(j=0;j< 4;j++){
+             printf("%d .ninci sutun ve %d'ninci satir",i,j);
+            scanf("%d",&matris[i][j]);
+
+        }
+        }
+
+        for(j=0;j < 4 ;j++){
+            for(i=0;i < 3;i++){
+                sum+=matris[i][j];
+
+
+                }
+            printf("%d 'ninci satirin toplami:%d\n",j,sum);
+            sum=0;
+        }
+
+
+
+
+
+
+    return 0;
+}
